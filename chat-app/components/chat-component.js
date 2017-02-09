9adcd16daf65efec901d2950e61ebe572401e2a9
katebennu <kate.dorrer@gmail.com>
2017-02-09 13:53:28 +0200

    rewrite chat-component as stateless functional component

--- chat-app/components/chat-component.js
+++ chat-app/components/chat-component.js
@@ -1,21 +1,19 @@
-var ChatApp = React.createClass({
+function ChatApp() {
+    var messages = state.messages.map(function (msg) {
+        return <li><strong>{msg.user}:</strong><span> {msg.body}</span></li>
+    });
 
-    render: function () {
-        var messages = this.state.messages.map(function (msg) {
-            return <li><strong>{msg.user}:</strong><span> {msg.body}</span></li>
-        });
+    return (
+        <div>
+            <ul>
+                {messages}
+            </ul>
+            <input id="message" type="text" value={this.state.message}/>
+            <button onClick={() => self.submitMessage()}>Bam!</button>
+            <input id="user" type="text" value={this.state.user} placeholder="pick a username"/>
+        </div>
+    )
+}
 
-        return (
-            <div>
-                <ul>
-                    {messages}
-                </ul>
-                <input id="message" type="text" value={this.state.message}/>
-                <button onClick={() => self.submitMessage()}>Bam!</button>
-                <input id="user" type="text" value={this.state.user} placeholder="pick a username"/>
-            </div>
-        )
-    }
-});
 
 module.exports = ChatApp;
